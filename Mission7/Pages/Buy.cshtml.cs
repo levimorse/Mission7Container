50e9db478999b110279f6a87d811dea009a24b03
levimorse <63205032+levimorse@users.noreply.github.com>
2022-04-19 20:25:40 -0600

    Sup

--- Mission7/Pages/Buy.cshtml.cs
+++ Mission7/Pages/Buy.cshtml.cs
@@ -15,9 +15,10 @@ namespace Mission7.Pages
 
         private IBookstoreRepository repo { get; set; }
 
-        public BuyModel (IBookstoreRepository temp)
+        public BuyModel (IBookstoreRepository temp, Basket b)
         {
             repo = temp;
+            basket = b;
         }
 
         public Basket basket { get; set; }
@@ -26,19 +27,26 @@ namespace Mission7.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+            //basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
         }
 
         public IActionResult OnPost(int bookId, string returnUrl)
         {
             Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
 
-            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+            //basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
             basket.AddItem(b, 1);
 
-            HttpContext.Session.SetJson("basket", basket);
+            //HttpContext.Session.SetJson("basket", basket);
 
             return RedirectToPage(new { ReturnUrl = returnUrl });
+        }
+
+        public IActionResult OnPostRemove (int bookId, string returnUrl)
+        {
+            basket.RemoveItem(basket.Items.First(x => x.Book.BookId == bookId).Book);
+
+            return RedirectToPage ( new { ReturnUrl = returnUrl } );
         }
     }
 }
